7f90f0c473ac375b8a391854f6ded9e72102ba3b
githubuiux <uiuxista@gmail.com>
2023-09-05 12:44:32 +0300

    Initial Commit

---
+++ CircleOnboarding/CircleOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleOnboardingApp.swift
+//  CircleOnboarding
+//
+//  Created by Levent on 5.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CircleOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
